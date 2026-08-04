02a0c22b532d6fd60dc758b48018bb1c23380f0b
Erlikx <firat@tehlike.net>
2026-08-04 02:25:28 +0000

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -83,7 +83,7 @@ APPS_CONFIG = {
     "google-photos": {
         "pkg": "com.google.android.apps.photos", "name": "google-photos", "patch_source": "rushi",
         "arch": "arm64-v8a", "icon": "https://cdn.simpleicons.org/googlephotos",
-        "force_version": "7.85.0.952162352",
+        "force_version": [],
         "exclude": [],
         "enable": [
             "AMOLED dark theme",
