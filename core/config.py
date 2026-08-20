85caf5911b511fee0e5f2b622da71c446ed75463
Erlikx <firat@tehlike.net>
2026-08-20 02:19:34 +0000

    Update config.py

--- core/config.py
+++ core/config.py
@@ -80,7 +80,8 @@ APPS_CONFIG = {
     },
     "warp": {
         "pkg": "com.cloudflare.onedotonedotonedotone", "name": "warp", "patch_source": "rushi",
-        "arch": "arm64-v8a", "icon": "https://cdn.simpleicons.org/1dot1dot1dot1", "exclude": [],
+        "arch": "arm64-v8a", "icon": "https://cdn.simpleicons.org/1dot1dot1dot1", 
+        "exclude": ["Disable SSL Pinning"],
     },
     "inshot": {
         "pkg": "com.camerasideas.instashot", "name": "inshot", "patch_source": "hooman",
