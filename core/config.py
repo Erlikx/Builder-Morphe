b750db9afac2aa71bb07ce537099ea99b23b5bda
Erlikx <firat@tehlike.net>
2026-09-05 16:14:01 +0000

    Update config.py

--- core/config.py
+++ core/config.py
@@ -156,7 +156,7 @@ APPS_CONFIG: dict[str, AppConfig] = {
     "tiktok": {
         "pkg": "com.zhiliaoapp.musically",
         "name": "tiktok",
-        "patch_source": ["tiktok", "morphe"],
+        "patch_source": "tiktok", "morphe",
         "arch": "arm64-v8a",
         "force_version": "46.2.3",
         "icon": "https://cdn.simpleicons.org/tiktok",
