0c97986c9e05ec44bc01527607230d752fc038cd
Erlikx <firat@tehlike.net>
2026-09-08 13:53:51 +0000

    Update apkmirror.py

--- core/sources/apkmirror.py
+++ core/sources/apkmirror.py
@@ -53,11 +53,11 @@ APP_SITES = {
         "release_slug": "termius-modern-ssh-client",
     },
     "lara-croft-gol": {"org": "feral-interactive", "slug": "lara-croft-guardian-of-light"},
-    "prince-of-persia-lost-crown": {"org": "ubisoft-entertainment", "slug": "prince-of-persia-lost-crown"},
+    "pop-lost-crown": {"org": "ubisoft-entertainment", "slug": "prince-of-persia-lost-crown"},
     "altos-adventure": {"org": "noodlecake-studios-inc", "slug": "altos-adventure"},
     "altos-odyssey": {"org": "noodlecake-studios-inc", "slug": "altos-odyssey"},
     "vivaldi-snapshot": {"org": "vivaldi-technologies", "slug": "vivaldi-browser-snapshot"},
-    "minecraft-pe-mods": {"org": "joycraft-mobile", "slug": "mods-addons-for-minecraft-pe"},
+    "mcpe-mods": {"org": "joycraft-mobile", "slug": "mods-addons-for-minecraft-pe"},
 }
 
 _CHROME_VERSION_RE = re.compile(r"(\d+\.\d+\.\d+\.\d+)")
