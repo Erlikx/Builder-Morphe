11b9d7cc7714f5ddc385f674f47a44f982198c31
Erlikx <firat@tehlike.net>
2026-09-08 20:22:02 +0000

    Update config.py

--- core/config.py
+++ core/config.py
@@ -117,7 +117,7 @@ APPS_CONFIG: dict[str, AppConfig] = {
         "name": "twitter",
         "patch_source": ["piko-newx", "morphe"],
         "arch": "arm64-v8a",
-        "force_version": "12.22.0-prod.01",
+        "force_version": "12.23.1-prod.01",
         "enable": ["Disable Play Store updates"],
         "icon": "https://cdn.simpleicons.org/x/000000",
     },
